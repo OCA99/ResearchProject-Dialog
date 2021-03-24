ffd16b9c7cb539356a199f2e18838eb0a332d847
Oscar Cuatrecasas <o.cuatrix@gmail.com>
2021-03-24 15:22:07 +0100

    Added handout

---
+++ handout/Game/Source/Scene.cpp
@@ -0,0 +1,65 @@
+#include "App.h"
+#include "Input.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Render.h"
+#include "Window.h"
+#include "Scene.h"
+
+#include "Defs.h"
+#include "Log.h"
+
+Scene::Scene() : Module()
+{
+	name.Create("scene");
+}
+
+// Destructor
+Scene::~Scene()
+{}
+
+// Called before render is available
+bool Scene::Awake()
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before the first frame
+bool Scene::Start()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::Update(float dt)
+{
+	return true;
+}
+
+// Called each loop iteration
+bool Scene::PostUpdate()
+{
+	bool ret = true;
+
+	if(app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	return ret;
+}
+
+// Called before quitting
+bool Scene::CleanUp()
+{
+	LOG("Freeing scene");
+
+	return true;
+}
